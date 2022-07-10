d9c6a362b60fc25035750311cf820a74e2c57f2c
Arthur <arthur.olkov@gmail.com>
2022-07-10 15:50:32 +0500

    Add Example010_Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 19, 28, 37, 64, 46, 55, 64, 73, 82, 91 };
+
+int n = array.Length;
+int find = 64;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+
+}
